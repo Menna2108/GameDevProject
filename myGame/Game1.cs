69613592242cae47907980f8bfb07c1e2a715d15
moula <moulahmina@gmail.com>
2024-12-06 03:24:02 +0100

    Adjusted rocket movement with collision detection, added background scrolling and changed background image

--- myGame/Game1.cs
+++ myGame/Game1.cs
@@ -10,10 +10,12 @@ namespace myGame
         private SpriteBatch _spriteBatch;
 
         private Texture2D backgroundTexture;
-        private Texture2D rocketSprite;        
-        private Vector2 rocketPosition;        
+        private Texture2D rocketSprite;
+        private Vector2 rocketPosition;
         private float rocketSpeed = 4f;
         private float rocketScale = 0.16f;
+        private float backgroundSpeed = 2f;
+        private float backgroundPositionY = 0;
 
         public Game1()
         {
@@ -24,7 +26,7 @@ namespace myGame
 
         protected override void Initialize()
         {
-            rocketPosition = new Vector2(100, 100);
+            rocketPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight - 100);
             base.Initialize();
         }
 
@@ -37,17 +39,36 @@ namespace myGame
 
         protected override void Update(GameTime gameTime)
         {
-            var keyboardState = Keyboard.GetState();  
+            var keyboardState = Keyboard.GetState();
 
-            // Beweging van mijn raket met pijltoetsen
-            if (keyboardState.IsKeyDown(Keys.Up))
-                rocketPosition.Y -= rocketSpeed;  
-            if (keyboardState.IsKeyDown(Keys.Down))
-                rocketPosition.Y += rocketSpeed;  
-            if (keyboardState.IsKeyDown(Keys.Left))
+            // Beweging van mijn raket
+            if (keyboardState.IsKeyDown(Keys.Up) && rocketPosition.Y > 0)  
+            {
+                rocketPosition.Y -= rocketSpeed;
+                backgroundPositionY += backgroundSpeed;  
+            }
+
+            // Collision detection voor omlaag, links en rechts
+            if (keyboardState.IsKeyDown(Keys.Down) && rocketPosition.Y + rocketSprite.Height * rocketScale < _graphics.PreferredBackBufferHeight)
+            {
+                rocketPosition.Y += rocketSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Left) && rocketPosition.X > 0)
+            {
                 rocketPosition.X -= rocketSpeed; 
-            if (keyboardState.IsKeyDown(Keys.Right))
-                rocketPosition.X += rocketSpeed;  
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Right) && rocketPosition.X + rocketSprite.Width * rocketScale < _graphics.PreferredBackBufferWidth)
+            {
+                rocketPosition.X += rocketSpeed; 
+            }
+
+            // Achtergrond reset naar boven wanneer deze onderaan het scherm komt
+            if (backgroundPositionY >= _graphics.PreferredBackBufferHeight)
+            {
+                backgroundPositionY = 0;  
+            }
 
             base.Update(gameTime);
         }
@@ -59,9 +80,10 @@ namespace myGame
             _spriteBatch.Begin();
 
             // Achtergrond schalen
-            _spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+            _spriteBatch.Draw(backgroundTexture, new Rectangle(0, (int)backgroundPositionY - _graphics.PreferredBackBufferHeight, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+            _spriteBatch.Draw(backgroundTexture, new Rectangle(0, (int)backgroundPositionY, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
 
-            // Raket tekenen
+            // Mijn raket tekenen
             _spriteBatch.Draw(rocketSprite, rocketPosition, null, Color.White, 0f, Vector2.Zero, rocketScale, SpriteEffects.None, 0f);
 
             _spriteBatch.End();
